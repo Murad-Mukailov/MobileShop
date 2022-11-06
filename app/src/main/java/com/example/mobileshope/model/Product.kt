495ded420ee7ca8827d59aa5944a1c24a61277e0
Murad <muradkartal91@gmail.com>
2022-11-06 19:10:50 +0300

    added module Product

---
+++ app/src/main/java/com/example/mobileshope/model/Product.kt
@@ -0,0 +1,12 @@
+package com.example.mobileshope.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Product(
+    @SerializedName("Kid") val id: Long,
+    @SerializedName("is_favorites") val isFavorite: Boolean,
+    @SerializedName("title") val title: String,
+    @SerializedName("price_without_discount") val rawPrice: Double,
+    @SerializedName("discount_price") val price: Double,
+    @SerializedName("picture") val image: String
+)
