6f884b719c5506024b4b67f153a7e556f2a8f448
Murad <muradkartal91@gmail.com>
2022-11-06 11:43:15 +0300

    added HomeViewModel

--- app/src/main/java/com/example/mobileshope/HomeActivity.kt
+++ app/src/main/java/com/example/mobileshope/HomeActivity.kt
@@ -1,11 +1,14 @@
 package com.example.mobileshope
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobileshope.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/mobileshope/view/model/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.mobileshope.view.model
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+}
