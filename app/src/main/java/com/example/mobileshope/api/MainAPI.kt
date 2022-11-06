cae395baaebacc376fa33ed1b2f9f80dc4452667
Murad <muradkartal91@gmail.com>
2022-11-06 19:10:15 +0300

    added interface MainAPI

---
+++ app/src/main/java/com/example/mobileshope/api/MainAPI.kt
@@ -0,0 +1,9 @@
+package com.example.mobileshope.api
+
+import com.example.mobileshope.model.HomeResponse
+import retrofit2.http.GET
+
+interface MainAPI {
+    @GET("654bd15e-b121-49ba-a588-960956b15175")
+    fun getHome(): HomeResponse
+}
