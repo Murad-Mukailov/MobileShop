68848e3b58a1ae797940022297a9b2ffb7ae5ffc
Murad <muradkartal91@gmail.com>
2022-11-08 21:14:33 +0300

    Create TitleDelegate

---
+++ app/src/main/java/com/example/mobileshope/home/delegate/TitleDelegate.kt
@@ -0,0 +1,22 @@
+package com.example.mobileshope.home.delegate
+
+import com.example.mobileshope.databinding.ItemTitleBinding
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
+
+data class TitleUIModel(
+    val text: String,
+    val action: String
+) : HomeUIModels(identification = text.hashCode().toLong())
+
+fun titleDelegate() = adapterDelegateViewBinding<TitleUIModel, HomeUIModels, ItemTitleBinding>(
+    viewBinding = { layoutInflater, parent ->
+        ItemTitleBinding.inflate(layoutInflater, parent, false)
+    }
+) {
+    with(binding) {
+        bind {
+            itemTitleText.text = item.text
+            viewTitleText.text = item.action
+        }
+    }
+}
