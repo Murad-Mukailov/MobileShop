6aa231907db6cfdad69b71a67c27b57c190d4e93
Murad <muradkartal91@gmail.com>
2022-11-13 11:44:45 +0300

    Create Data Class Sale

---
+++ app/src/main/java/com/example/mobileshope/model/Sale.kt
@@ -0,0 +1,12 @@
+package com.example.mobileshope.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Sale(
+    @SerializedName("id") val id: Long,
+    @SerializedName("is_new") val new: Boolean,
+    @SerializedName("title") val name: String,
+    @SerializedName("subtitle") val description: String,
+    @SerializedName("picture") val image: String,
+    @SerializedName("is_buy") val isBuy: Boolean
+)
