68eac1e98d277fc7cbf4678022219bb5e5c3ab68
Murad <muradkartal91@gmail.com>
2022-11-16 08:04:41 +0300

    Create products list and product delegate

---
+++ app/src/main/java/com/example/mobileshope/home/delegate/ProducstListDelegate.kt
@@ -0,0 +1,27 @@
+package com.example.mobileshope.home.delegate
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mobileshope.databinding.ItemProductListBinding
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
+
+data class ProductListUIModel<T : HomeUIModels>(
+
+    override val identification: Long,
+    val list: List<T>
+) : HomeUIModels(identification)
+
+fun productListDelegate(
+    recyclerFactory: RecyclerView.(ProductListUIModel<*>) -> Unit,
+) = adapterDelegateViewBinding<ProductListUIModel<*>, HomeUIModels, ItemProductListBinding>(
+    viewBinding = { layoutInflater, parent ->
+        ItemProductListBinding.inflate(layoutInflater, parent, false)
+    }
+) {
+    with(binding) {
+        bind {
+            recyclerViewProduct.recyclerFactory(item)
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/mobileshope/home/delegate/ProductDelegate.kt
@@ -0,0 +1,34 @@
+package com.example.mobileshope.home.delegate
+
+import android.graphics.Paint
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.example.mobileshope.databinding.ItemProductBinding
+import com.example.mobileshope.model.Product
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
+
+data class ProductUIModel(override val data: Product) :
+    HomeUIModels(identification = data.id)
+
+fun productDelegate() =
+    adapterDelegateViewBinding<ProductUIModel, HomeUIModels, ItemProductBinding>(
+        viewBinding = { layoutInflater, parent ->
+            ItemProductBinding.inflate(layoutInflater, parent, false)
+        }
+
+    ) {
+        with(binding) {
+            bind {
+                Glide.with(favoriteProductImage)
+                    .load(item.data.image)
+                    .into(favoriteProductImage)
+                nameProductText.text = item.data.title
+                favoriteProductIcon.isVisible = item.data.isFavorite
+                discountPriceText.text = item.data.rawPrice.toString()
+                discountPriceText.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                priceProductText.text = item.data.price.toString()
+
+
+            }
+        }
+    }
