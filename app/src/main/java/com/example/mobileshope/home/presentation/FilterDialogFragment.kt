49f18bf579b353bc569ea2133bd5acf789798008
Murad <muradkartal91@gmail.com>
2023-01-08 18:57:57 +0300

    "Home" - created dialog fragment

---
+++ app/src/main/java/com/example/mobileshope/home/presentation/FilterDialogFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mobileshope.home.presentation
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class FilterDialogFragment : BottomSheetDialogFragment() {
+
+}
